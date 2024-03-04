68d7c3f542be3a16037745078df20ed1782af2cf
balaji reddy <71547771+balaji-reddy-helper@users.noreply.github.com>
2024-03-04 11:14:08 +0530

    Create swap.cpp

---
+++ POTD/swap_the_array/swap.cpp
@@ -0,0 +1,70 @@
+/*
+Given an array arr of n positive integers. The task is to swap every ith element of the array with (i+2)th element.
+
+Input:
+n = 3
+arr[] = 1 2 3
+Output:
+3 2 1
+Explanation:
+Swapping 1 and 3, makes the array 3 2 1. There is only one swap possible in this array.
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Function to swap array elements
+* arr : array input
+* n : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        // Your code here
+        for(int i=0;i<n-2;i++)
+        {
+            swap(arr[i],arr[i+2]);  
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code to test swapElements
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int n;
+	    cin >> n;
+	    
+	    int arr[n];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr[i];
+	    }
+        Solution obj;
+        // calling function to swap the array swap elements
+	    obj.swapElements(arr, n);
+	    
+	    // Printing the modified array
+	    for(int i = 0;i<n;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
